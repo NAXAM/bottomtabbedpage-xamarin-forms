cf0dc1c48e12f6d4e5b4fa493fc827ddccc99c36
bình đỗ thanh <bnh  thanh>
2017-04-26 18:19:00 +0700

    add new Project

---
+++ Demo/Demo/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Naxam.BottomNavs.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo
+{
+	public partial class MainPage : BottomTabbedPage
+    {
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
