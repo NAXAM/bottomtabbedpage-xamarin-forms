1f367b9efcd3b0cb8dc23e78c2bc139c53fbb82a
Vu Duc Tuyen <tuyen@naxam.net>
2017-05-07 11:33:37 +0700

    Initial

---
+++ BottomTabbedPage.Demo/BottomTabbedPage.DemoPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BottomTabbedPage.Demo
+{
+    public partial class BottomTabbedPage_DemoPage : ContentPage
+    {
+        public BottomTabbedPage_DemoPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Droid/MainActivity.cs
@@ -0,0 +1,23 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace BottomTabbedPage.Demo.Droid
+{
+    [Activity(Label = "Bottom Tabbed", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            LoadApplication(new App());
+        }
+    }
+}
